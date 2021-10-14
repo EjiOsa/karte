52a5ac45f33da668ea9e10eb8a0fa5bcf6ce2b2b
ejiosa <withthenature@gmail.com>
2021-10-14 19:05:26 +0900

    更新処理をリファクタリング

--- backend/applications/staff/serializer.py
+++ backend/applications/staff/serializer.py
@@ -1,3 +1,4 @@
+from django.db.models.enums import Choices
 from rest_framework import serializers
 from .models import *
 from applications.master.models import Role
@@ -21,24 +22,20 @@ class DoctorSerializer(serializers.ModelSerializer):
         validated_data['role'] = validated_data.get('role_uid', None)
 
         if validated_data['role'] is None:
-            raise serializers.ValidationError("role not found.")
+            raise serializers.ValidationError("role not found of CREATE.")
 
         del validated_data['role_uid']
         return Doctor.objects.create(**validated_data)
 
-    # PUT時に外部キーを変更できなかった、これが正解かは不明だが更新可能になっている。
+    # オーバーライドでは他の項目も必要なので、updateを継承して処理追加
     def update(self, instance, validated_data):
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name_kana = validated_data.get('last_name_kana', instance.last_name_kana)
-        instance.first_name_kana = validated_data.get('first_name_kana', instance.first_name_kana)
-        instance.age = validated_data.get('age', instance.age)
-        instance.sex = validated_data.get('sex', instance.sex)
         instance.role = validated_data.get('role_uid', None)
-        instance.save()
+
+        if instance.role is None:
+            raise serializers.ValidationError("role not found of UPDATE.")
 
         del validated_data['role_uid']
-        return instance
+        return super().update(instance, validated_data)
 
     class Meta:
         model = Doctor
